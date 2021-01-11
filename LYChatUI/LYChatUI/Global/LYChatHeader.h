2d3e8851fe429c79fb60106957ab0a4f052ffd10
luoyuant <314548713@qq.com>
2021-01-11 10:23:42 +0800

    update

--- LYChatUI/LYChatUI/Global/LYChatHeader.h
+++ LYChatUI/LYChatUI/Global/LYChatHeader.h
@@ -12,7 +12,7 @@
 #define LYDLog( s, ... ) printf("class: <%p %s:(%d) > method: %s \n%s\n", self, [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, __PRETTY_FUNCTION__, [[NSString stringWithFormat:(s), ##__VA_ARGS__] UTF8String] )
 //#define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
 #else
-#define DLog(...)
+#define LYDLog(...)
 #endif
 
 #endif /* LYChatHeader_h */
